083a4978cff1052a54fdb443b124759e141616d7
tofsona14 <topuria2074@gmail.com>
2024-04-18 16:46:35 +0400

    add almost done app

---
+++ Practise/app.js
@@ -0,0 +1,24 @@
+var a = (x) => {
+    return x * x
+}
+const yuti = 451
+a(yuti)
+var b = Math.sqrt(yuti)
+console.log(`${yuti} აყვანილი კვადრატში არის ${a(yuti)}`)
+
+const c = [451, 677, 1999,2003,102]
+c.map((e, i) => {
+    console.log(i)
+}
+
+
+) 
+
+var h = 15
+var j = 20
+var te = (o,s) => {
+    return o+s
+
+}
+const op = h+j
+console.log(op)
